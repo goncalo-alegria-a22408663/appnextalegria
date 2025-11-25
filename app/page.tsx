fbd2160ac55c5503a261238c47b237e603ebbca0
Goncalo Alegria <a22408663@alunos.ulht.pt>
2025-11-25 12:47:46 +0000

    commit

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import MagiaDoJSX from "@/components/MagiaDoJSX/MagiaDoJSX"
+import Link from "next/link"
+
+export default function page() {
+
+  return (
+    <div>
+      <header className="flex flex-col items-center">
+        <h1>React & Next.js</h1>
+        <nav className="flex gap-4">
+          <Link href="/">Intro</Link>
+          <Link href="/sobre">Sobre</Link>
+          <Link href="/caracteristicas">Características</Link>
+          <Link href="/tecnologias">Tecnologias</Link>
+
+        </nav>
+      </header>
+      <h2>Interfaces Modernos</h2>
+      <p>Bem vindo à minha App em React e Next.js</p>
+      <MagiaDoJSX/>
+    </div>
+  )
+}
